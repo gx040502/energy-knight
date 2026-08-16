297a5444d18dfe2612ff935a3fa5a18269ee5ec1
sinyen04 <yeesinyen13@gmail.com>
2026-08-16 21:50:00 +0800

    pause menu

--- Sinyen/levelSelection/scripts/level_selection.gd
+++ Sinyen/levelSelection/scripts/level_selection.gd
@@ -18,19 +18,19 @@ func _on_level_1_button_pressed():
 
 
 func _on_level_2_button_pressed():
-	get_tree().change_scene_to_file("res://Sinyen/scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Sinyen/levelSelection/scenes/level_1.tscn")
 	
 
 
-func _on_level_3_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Sinyen/scenes/level_1.tscn")
+func _on_level_3_button_pressed():
+	get_tree().change_scene_to_file("res://Sinyen/levelSelection/scenes/level_1.tscn")
 
 
 
-func _on_level_4_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Sinyen/scenes/level_1.tscn")
+func _on_level_4_button_pressed():
+	get_tree().change_scene_to_file("res://Sinyen/levelSelection/scenes/level_1.tscn")
 
 
 
 func _on_back_button_pressed():
-	get_tree().change_scene_to_file("res://Sinyen/main_menu.tscn")
+	get_tree().change_scene_to_file("res://Sinyen/main_menu/scenes/main_menu.tscn")
